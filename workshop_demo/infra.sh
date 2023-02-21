ac33b591e1d93895ac30ac471164af715e572745
mohamed-chargui <mouhammed.chargui@gmail.com>
2023-02-21 05:26:59 -0500

    add user to docker group in the  script

--- workshop_demo/infra.sh
+++ workshop_demo/infra.sh
@@ -1,8 +1,8 @@
 #Installing the Default JRE/JDK
 #update the cache
-sudo apt update 
-sudo apt install default-jre
-sudo apt install default-jdk
+sudo apt update ,-y 
+sudo apt install default-jre -y
+sudo apt install default-jdk -y
 
 #install jenkins 
 #First, add the repository key to the system
@@ -10,8 +10,8 @@ wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key 
 
 #append the Debian package repository address to the server’s sources.list
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update
-sudo apt install jenkins
+sudo apt update -y
+sudo apt install jenkins -y
 
 #start and enable jenkins 
 sudo systemctl start jenkins 
@@ -19,18 +19,18 @@ sudo systemctl enable jenkins
 sudo systemctl status jenkins 
 
 #in case the firewall is closed 
-#sudo ufw allow 8080
-#sudo ufw allow OpenSSH
-#sudo ufw enable
-#sudo ufw status
+sudo ufw allow 8080
+sudo ufw allow OpenSSH
+sudo ufw enable
+sudo ufw status
 
 #install ansible 
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt update
-sudo apt install ansible
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt update -y
+sudo apt install ansible -y
 
 #install docker 
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install \
     ca-certificates \
     curl \
@@ -44,7 +44,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 #Install Docker Engine
-sudo apt-get update
+sudo apt-get update -y
 #Receiving a GPG error when running apt-get update
 #sudo chmod a+r /etc/apt/keyrings/docker.gpg
 #sudo apt-get update
@@ -65,6 +65,9 @@ sudo chmod +x /usr/local/bin/minikube
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
+
+#add user to docker group 
+sudo usermod -aG docker $USER && newgrp docker
 
 #start minikube 
 minikube start --driver=docker
